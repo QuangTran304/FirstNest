3621c4943b0b1cd403ab40482ebc650c9aab188f
Quang Tran <nhatquang304@gmail.com>
2023-07-01 14:32:34 -0400

    🔨 created CRUD routes

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  // GET /ninjas --> []
+  @Get()
+  getNinjas() {
+    return [];
+  }
+
+  // GET /ninjas/:id --> {}
+  @Get(':id')
+  getOneNinja(@Param('id') id: string) {
+    return {
+      id,
+    };
+  }
+
+  // POST /ninjas --> {}
+  @Post(':id')
+  createNinja() {
+    return {};
+  }
+
+  // PUT /ninjas/:id --> {}
+  @Put(':id')
+  updateNinja() {
+    return {};
+  }
+
+  // DELETE /ninjas/:id --> {}
+  @Delete(':id')
+  deleteNinja() {
+    return {};
+  }
+}
